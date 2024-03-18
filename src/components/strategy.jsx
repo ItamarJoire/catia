bbf4ab13c947df6ecdd230db79607fcdea870d06
Itamar Joire <itamar.joire@imegi.local>
2024-03-18 16:58:08 -0300

    margin

--- src/components/strategy.jsx
+++ src/components/strategy.jsx
@@ -5,10 +5,10 @@ import ImgCatia from '/catia.png'
 export function Strategy(){
   return(
     <GridContainer>
-      <div className="my-16 md:flex md:justify-around">
-        <img src={ImgCatia} alt="" className="w-[600px] h-auto mx-auto md:mx-0"/>
+      <div className="my-28 md:flex md:justify-around">
+        <img src={ImgCatia} alt="" className="w-[500px] h-auto mx-auto md:mx-0"/>
         
-        <div className="mt-4">
+        <div className="mt-24">
           <p className="text-sm uppercase font-light text-gray-700">Como Cátia Oliveira pode te ajudar?</p>
           <h2><span className="text-base-green text-xl">Aplicar as estratégias corretas</span>...</h2>
           <p className="text-gray-600 md:max-w-[500px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum placeat blanditiis atque magni voluptatem. Similique, odio temporibus velit consectetur dolor minima veniam culpa repellat laborum! Nihil esse ea sequi rem.
