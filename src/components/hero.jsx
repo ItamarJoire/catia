88ee0c99c5170e0aeb261c56439412b20a7938b8
Itamar Joire <itamar.joire@imegi.local>
2024-03-21 09:38:26 -0300

    Text Hero

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -15,7 +15,7 @@ export function Hero(){
               A sua jornada de superação começa agora! 
               </h1>
               <h4 className='mt-3 mb-8 text-xl text-center text-gray-500 max-auto md:text-2xl lg:text-left'>
-              Recomece sua vida com uma nova perspectiva: uma mulher de atitude é madura, e disposta a se conhecer a ressignificar. 
+              Recomece sua vida com uma nova perspectiva: uma mulher de atitude é madura, é disposta a se conhecer e ressignificar. 
               </h4>
             
               <a href="https://api.whatsapp.com/send?l=pt&phone=5571992360876&text=Olá! Tudo bem?" className='font-["Poppins"] block px-8 py-5 max-w-[250px] mx-auto bg-gradient-to-r from-base-yellow-dark to-base-green-light text-white font-bold rounded-md hover:opacity-90 duration-200 uppercase md:text-xl md:max-w-[320px] md:px-10 md:py-6 lg:mx-0'>
