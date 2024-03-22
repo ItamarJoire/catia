0d01b5c0b5693e141c0a5bc403ee7e6949ee3a0c
Itamar Joire <itamar.joire@imegi.local>
2024-03-22 13:33:12 -0300

    Image

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,13 +1,13 @@
 import { GridContainer } from "../containers";
 
-import ImgMentor from '/mentor.jpeg'
+import ImgMentor from '/mentor.png'
 
 export function About(){
   return(
     <GridContainer>
       <div className="">
         <div>
-          <img src={ImgMentor} alt="" className="mb-8 rounded-md mx-auto md:max-w-[400px] lg:size-[460px] lg:float-right lg:ml-6 lg:mt-24"/>
+          <img src={ImgMentor} alt="" className="mb-8 rounded-md mx-auto w-[300px] md:max-w-[400px] lg:w-[360px] lg:float-right lg:ml-6 lg:mt-24"/>
           <p className="text-sm uppercase font-medium text-gray-700 md:text-lg">Conheça um pouco da sua mentora</p>
           <h2><span className="text-base-green text-xl md:text-2xl">Cátia Oliveira </span></h2>
           <p className="text-gray-500 text-base pt-2 md:text-lg lg:max-w-[1000px]">Renomada empresária, escritora e palestrante, emerge como uma voz inspiradora para mulheres maduras que enfrentam desafios. Com 50 anos, mãe solo e cristã dedicada, Cátia é uma especialista em Gestão Empresarial e Master Coach, desempenhando o papel de mentora para mulheres que buscam superações significativas.
