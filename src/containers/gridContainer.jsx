ead5e20fb879e4b4d71337587cd0514ff149cc7e
Itamar Joire <ijoire7@gmail.com>
2024-04-17 11:32:15 -0300

    Responsive

--- src/containers/gridContainer.jsx
+++ src/containers/gridContainer.jsx
@@ -1,6 +1,6 @@
 export function GridContainer( { children } ){
   return(
-    <div className="mx-3 md:mx-10 lg:mx-36">
+    <div className="mx-3 md:mx-10 lg:mx-36 xl:mx-96">
       { children }
     </div>
   )
